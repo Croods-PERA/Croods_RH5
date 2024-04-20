f3e06ed4d5151146ccdb590f059436a62f03c818
Oneplus-2997 <mario808@hotmail.com>
2024-04-20 13:59:56 +0000

    Add HOST

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,24 @@ import cors from 'cors';
 import { config } from 'dotenv';
 import { dbConnection } from "./database/dbConnection.js";
 
-const app = express();
+
 config({ path: "./config.env" });
 
-const PORT = process.env.PORT || 5000;
+
+const HOST = process.env.APP_HOST
+const PORT = process.env.APP_PORT || 5000;
+
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// test
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
 
 // Connect to MongoDB
 dbConnection();
